1a6893b35f56a00090a30585ed89013cdcdcae56
akihiro.tanaka <big.a.0816@gmail.com>
2022-04-21 22:55:36 +0900

    fix all test codes to make them work successfully

---
+++ packages/chainlink-external-adapter/jest.config.ts
@@ -0,0 +1,16 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig');
+
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['src/**/*.ts', '!**/*.module.ts', '!**/index.ts'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
+};
--- packages/chainlink-external-adapter/src/api/filecoin/filecoin.controller.spec.ts
+++ packages/chainlink-external-adapter/src/api/filecoin/filecoin.controller.spec.ts
@@ -1,18 +1,51 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { FilecoinLotusRepository } from '@shared/repositories';
+import { ConfigModule } from '@shared/modules';
+import { Response } from 'express';
+
 import { FilecoinController } from './filecoin.controller';
+import { GetMessageDto } from './dtos/get-message.dto';
+import { FilecoinService } from './filecoin.service';
 
 describe('FilecoinController', () => {
   let controller: FilecoinController;
+  let filecoinService: FilecoinService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule],
       controllers: [FilecoinController],
+      providers: [
+        FilecoinService,
+        {
+          provide: FilecoinLotusRepository,
+          useValue: jest.fn(),
+        },
+      ],
     }).compile();
 
     controller = module.get<FilecoinController>(FilecoinController);
+    filecoinService = module.get<FilecoinService>(FilecoinService);
+
+    jest
+      .spyOn(filecoinService, 'fetchMessage')
+      .mockImplementation(() => Promise.resolve('testMsg'));
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('root', () => {
+    it('should return data', async () => {
+      const response = {
+        redirect: (_: number, __: string) => {},
+      } as Response;
+
+      expect(controller.root(response)).toBeUndefined();
+    });
+  });
+
+  describe('fetchMessage', () => {
+    it('should return data', async () => {
+      const message: GetMessageDto = { messageId: { '/': 'testId' } };
+      expect(await controller.fetchMessage(message)).toBe('testMsg');
+    });
   });
 });
