9ab3f780450218d3468e4587f075c94fbb564962
akihiro.tanaka <big.a.0816@gmail.com>
2022-05-11 12:26:01 +0900

    fix config module settings

--- packages/chainlink-external-adapter/src/shared/modules/config/config.util.ts
+++ packages/chainlink-external-adapter/src/shared/modules/config/config.util.ts
@@ -1,6 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { ConfigService, Path, PathValue } from '@nestjs/config';
 import { IEnvironmentVariables } from './environment-variables';
 
 @Injectable()
-export class ConfigUtil extends ConfigService<IEnvironmentVariables> {}
+export class ConfigUtil extends ConfigService<IEnvironmentVariables> {
+  get<
+    P extends Path<IEnvironmentVariables>,
+    R = PathValue<IEnvironmentVariables, P>,
+  >(propertyPath: P): R | undefined {
+    return super.get(propertyPath, { infer: true });
+  }
+}
