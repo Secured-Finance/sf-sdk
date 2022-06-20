1acf73341ffbff314fd647e403b3e0b48942bc16
bahadylbekov <baha296@mail.ru>
2022-06-20 23:51:24 +0600

    fix: graph-client close-out-netting test
    SF-79

--- packages/sf-graph-client/test/hooks/close-out.test.ts
+++ packages/sf-graph-client/test/hooks/close-out.test.ts
@@ -20,6 +20,26 @@ describe('useCloseOutNetting hook test', () => {
         expect(result.error?.message).to.contain('invalid address');
     });
 
+    it('Should throw an error on empty counterparty address', async () => {
+        const { result } = renderHook(() => useCloseOutNetting(user, '', ccy));
+
+        expect(result.error?.message).to.contain('invalid address');
+    });
+
+    it('Should return undefined on empty currency identifier', async () => {
+        const { result } = renderHook(() =>
+            useCloseOutNetting(user, counterparty, '')
+        );
+
+        await new Promise<void>(res =>
+            setTimeout(() => {
+                res();
+            }, 1000)
+        );
+
+        expect(result.current.data?.closeOutNetting).to.be.undefined;
+    });
+
     it('Should get data for existing CloseOutNetting from subgraph', async () => {
         const { result } = renderHook(() =>
             useCloseOutNetting(user, counterparty, ccy)
@@ -33,7 +53,7 @@ describe('useCloseOutNetting hook test', () => {
 
         if (result.current.data?.closeOutNetting !== undefined) {
             validateCloseOutNetting(result.current.data.closeOutNetting);
-            validateCurrency(result.current.data.closeOutNetting.currency);
+            validateCurrency(result.current.data?.closeOutNetting?.currency);
         }
     });
 });
