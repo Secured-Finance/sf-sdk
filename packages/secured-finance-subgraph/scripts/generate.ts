2bf0f6a4859d630b856b0a4a70bcfd4bf3da12a4
akihiro tanaka <big.a.0816@gmail.com>
2022-07-20 22:09:45 +0900

    [SF-18] Fix a deployment flow for the subgraph

--- packages/secured-finance-subgraph/scripts/generate.ts
+++ packages/secured-finance-subgraph/scripts/generate.ts
@@ -1,41 +1,43 @@
-import {
-  readFileSync, writeFileSync
-} from 'fs';
+import { readFileSync, writeFileSync } from 'fs';
 import { dump, load } from 'js-yaml';
 
 class Main {
-  private environment: string;
-  private arrowedEnvironments = ['development', 'staging', 'production'];
+    private environment: string;
+    private arrowedEnvironments = ['development', 'staging', 'production'];
 
-  constructor(environment: string) {
-      if (!this.arrowedEnvironments.includes(environment)) {
-          console.error('Invalid environment:', environment);
-          process.exit(1);
-      }
-      this.environment = environment;
-  }
+    constructor(environment: string) {
+        if (!this.arrowedEnvironments.includes(environment)) {
+            console.error('Invalid environment:', environment);
+            process.exit(1);
+        }
+        this.environment = environment;
+    }
 
-  run() {
-    const rootDir = process.cwd();
-    const contractFolderDir = `${rootDir}/node_modules/@secured-finance/smart-contracts/deployments/${this.environment}`;
+    async run() {
+        const rootDir = process.cwd();
+        const yamlText = readFileSync(`${rootDir}/subgraph.yaml`, 'utf8');
+        const data = load(yamlText) as any;
 
-    const yamlText = readFileSync(`${rootDir}/subgraph.yaml`, 'utf8');
-    const data = load(yamlText) as any;
+        for (const dataSource of data.dataSources) {
+            const deployment = await import(
+                `@secured-finance/smart-contracts/deployments/${this.environment}/${dataSource.name}.json`
+            );
 
-    data.dataSources.forEach((data: any) => {
-        const contractFile = `${contractFolderDir}/${data.name}.json`;
-        const deployment = JSON.parse(readFileSync(contractFile, 'utf8'));
-        const proxyAddress = deployment.address;
-        const blockNumber = deployment.receipt.blockNumber;
+            const proxyAddress = deployment.address;
+            const blockNumber = deployment.receipt.blockNumber;
 
-        data.source.address = proxyAddress;
-        data.source.startBlock = blockNumber;
-    });
+            dataSource.source.address = proxyAddress;
+            dataSource.source.startBlock = blockNumber;
+        }
 
-    const newYamlText = dump(data);
+        const newYamlText = dump(data);
 
-    writeFileSync(`${rootDir}/subgraph.${this.environment}.yaml`, newYamlText, 'utf8');
-  }
+        writeFileSync(
+            `${rootDir}/subgraph.${this.environment}.yaml`,
+            newYamlText,
+            'utf8'
+        );
+    }
 }
 
 const [, , environment] = process.argv;
