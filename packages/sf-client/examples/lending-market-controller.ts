4bb1d90381aa609a34dfddda4797647ca5c6175d
akihiro tanaka <big.a.0816@gmail.com>
2022-09-20 14:07:36 +0900

    fix: update smart-contracts version and add approval steps [SF-210]

--- packages/sf-client/examples/lending-market-controller.ts
+++ packages/sf-client/examples/lending-market-controller.ts
@@ -1,33 +1,13 @@
-import { Token } from '@secured-finance/sf-core';
 import 'dotenv/config';
 import { providers, Wallet } from 'ethers';
 import { SecuredFinanceClient } from '../src';
-
-export class Filecoin extends Token {
-    private constructor() {
-        super(
-            1,
-            '0x0000000000000000000000000000000000000000',
-            18,
-            'FIL',
-            'Filecoin'
-        );
-    }
-
-    private static instance: Filecoin;
-
-    public static onChain(): Filecoin {
-        this.instance = this.instance || new Filecoin();
-        return this.instance;
-    }
-}
+import { Filecoin } from './tokens/filecoin';
 
 (async function () {
     const provider = new providers.JsonRpcProvider(
-        `https://rinkeby.infura.io/v3/${process.env.INFURA_KEY}`,
-        'rinkeby'
+        process.env.DEV_RPC_ENDPOINT
     );
-    let wallet = new Wallet(`0x${process.env.PRIVATE_KEY}`);
+    let wallet = new Wallet(`0x${process.env.DEV_PRIVATE_KEY}`);
     wallet = wallet.connect(provider);
 
     let network = await provider.getNetwork();
@@ -38,4 +18,12 @@ export class Filecoin extends Token {
 
     const lendingMarkets = await sfClient.getLendingMarkets(FIL);
     console.table(lendingMarkets);
+
+    await sfClient.placeLendingOrder(
+        FIL,
+        lendingMarkets[0].maturity,
+        '0',
+        1000,
+        100000
+    );
 })();
