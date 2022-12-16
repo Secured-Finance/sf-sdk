73c9758d793872895ed9f3ee6b40e0e08febd809
Reuven Aboulker <reuven.aboulker@gmail.com>
2022-12-16 21:05:50 +0800

    style: change name of enum and export path
    [SF-247]

--- packages/sf-client/src/index.ts
+++ packages/sf-client/src/index.ts
@@ -1,5 +1,5 @@
 import { LendingMarketInfo, SecuredFinanceClientConfig } from './entities';
-import { SecuredFinanceClient } from './secured-finance-client';
+import { OrderSide, SecuredFinanceClient } from './secured-finance-client';
 import * as utils from './utils';
 
 export {
@@ -7,4 +7,5 @@ export {
     utils,
     SecuredFinanceClientConfig,
     LendingMarketInfo,
+    OrderSide,
 };
