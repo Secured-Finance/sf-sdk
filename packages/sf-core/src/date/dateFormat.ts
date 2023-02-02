cdc441c8bc0b948ad2750fb806f257f7a4065413
Arpit Singh <arpit@secured-finance.com>
2023-02-02 17:44:14 +0530

    address pr reviews

--- packages/sf-core/src/date/dateFormat.ts
+++ packages/sf-core/src/date/dateFormat.ts
@@ -1,17 +1,17 @@
 import * as dayjs from 'dayjs';
 
-export const getUTCMonthYear = (timestamp: number, multiplier = 1000) => {
-    const date = new Date(timestamp * multiplier);
+export const getUTCMonthYear = (timestamp: number) => {
+    const date = new Date(timestamp * 1000);
     return dayjs(date.toUTCString()).format('MMMYY').toUpperCase();
 };
 
-export const formatDate = (timestamp: number, multiplier = 1000) => {
+export const formatDate = (timestamp: number) => {
     return timestamp
         ? Intl.DateTimeFormat('en-US', {
               year: 'numeric',
               month: 'short',
               day: 'numeric',
               timeZone: 'GMT',
-          }).format(new Date(timestamp * multiplier))
+          }).format(new Date(timestamp * 1000))
         : '';
 };
--- packages/sf-core/src/entities/index.ts
+++ packages/sf-core/src/entities/index.ts
@@ -1,6 +1,5 @@
 export * from './baseCurrency';
 export * from './currency';
-export * from './dateFormat';
 export * from './ether';
 export * from './nativeCurrency';
 export * from './token';
