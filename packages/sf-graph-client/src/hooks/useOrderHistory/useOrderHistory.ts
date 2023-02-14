bfd0b093f9f5c9fbac3656825ba6c87005358cea
Reuven Aboulker <reuven.aboulker@gmail.com>
2023-02-14 11:00:40 +0000

    feat: update queries for latest graph [SF-301]

--- packages/sf-graph-client/src/hooks/useOrderHistory/useOrderHistory.ts
+++ packages/sf-graph-client/src/hooks/useOrderHistory/useOrderHistory.ts
@@ -1,28 +1,29 @@
+import {
+    UserOrderHistoryDocument,
+    UserOrderHistoryQuery,
+} from 'src/graphclient/.graphclient';
 import { GraphApolloClient } from '../../components';
-import { OrderHistoryDocument, OrderHistoryQuery } from '../../graphclient';
 import { AccountVariable } from '../types';
 import { QueryResult, useQuery } from '../useQuery';
 
 export const useOrderHistory = (
     { account }: AccountVariable,
     client?: GraphApolloClient
-): QueryResult<OrderHistoryQuery> => {
+): QueryResult<UserOrderHistoryQuery> => {
     const variables = {
         address: account.toLowerCase(),
         awaitRefetchQueries: true,
     };
 
-    const { error, data, refetch, networkStatus } = useQuery<OrderHistoryQuery>(
-        OrderHistoryDocument,
-        {
+    const { error, data, refetch, networkStatus } =
+        useQuery<UserOrderHistoryQuery>(UserOrderHistoryDocument, {
             variables: variables,
             client: client,
             fetchPolicy: 'network-only',
             notifyOnNetworkStatusChange: true,
-        }
-    );
+        });
 
-    const isExists = data?.orders;
+    const isExists = data?.user;
 
     return {
         data: isExists ? data : undefined,
--- packages/sf-graph-client/src/hooks/useTransactionHistory/useTransactionHistory.ts
+++ packages/sf-graph-client/src/hooks/useTransactionHistory/useTransactionHistory.ts
@@ -1,7 +1,7 @@
 import { GraphApolloClient } from '../../components';
 import {
-    TransactionHistoryDocument,
-    TransactionHistoryQuery,
+    UserTransactionHistoryDocument,
+    UserTransactionHistoryQuery,
 } from '../../graphclient';
 import { AccountVariable } from '../types';
 import { QueryResult, useQuery } from '../useQuery';
@@ -9,21 +9,21 @@ import { QueryResult, useQuery } from '../useQuery';
 export const useTransactionHistory = (
     { account }: AccountVariable,
     client?: GraphApolloClient
-): QueryResult<TransactionHistoryQuery> => {
+): QueryResult<UserTransactionHistoryQuery> => {
     const variables = {
         address: account.toLowerCase(),
         awaitRefetchQueries: true,
     };
 
     const { error, data, refetch, networkStatus } =
-        useQuery<TransactionHistoryQuery>(TransactionHistoryDocument, {
+        useQuery<UserTransactionHistoryQuery>(UserTransactionHistoryDocument, {
             variables: variables,
             client: client,
             fetchPolicy: 'network-only',
             notifyOnNetworkStatusChange: true,
         });
 
-    const isExists = data?.transactions;
+    const isExists = data?.user;
 
     return {
         data: isExists ? data : undefined,
