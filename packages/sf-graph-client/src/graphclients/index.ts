a14cb027cec0781828c8ae144c2a42ad155902f5
NetFreak26 <chirayugarg001999@gmail.com>
2023-05-11 09:44:50 +0530

    fix: queries type should be set to union of dev_queries and staging_queries [SF-363]

--- packages/sf-graph-client/src/graphclients/index.ts
+++ packages/sf-graph-client/src/graphclients/index.ts
@@ -2,7 +2,7 @@ import * as DEV_QUERIES from './development/.graphclient';
 import * as STAGING_QUERIES from './staging/.graphclient';
 
 const getQueries = () => {
-    let queries;
+    let queries: typeof DEV_QUERIES | typeof STAGING_QUERIES;
     switch (process.env.SF_ENV) {
         case 'development':
             queries = DEV_QUERIES;
@@ -12,7 +12,7 @@ const getQueries = () => {
             break;
         case 'production':
         default:
-            // TODO: set the queries to Prod Queries for production env
+            // TODO: set the queries to Prod Queries for production
             break;
     }
     return queries;
