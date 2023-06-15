c29511e1441d0abcf2e0fecd0659f9b49633f9d0
Reuven Aboulker <reuven.aboulker@gmail.com>
2023-06-15 14:15:41 +0100

    fix: fix return type [SF-568]

--- packages/sf-graph-client/src/hooks/useQuery.ts
+++ packages/sf-graph-client/src/hooks/useQuery.ts
@@ -1,5 +1,4 @@
 import {
-    ApolloQueryResult,
     DocumentNode,
     OperationVariables,
     QueryHookOptions,
@@ -7,12 +6,10 @@ import {
     useQuery as useApolloQuery,
 } from '@apollo/client';
 
-export type QueryResult<T> = ApolloQueryResult<T>;
-
 function useQuery<TData, TVariables = OperationVariables>(
     query: DocumentNode | TypedDocumentNode<TData, TVariables>,
     options?: QueryHookOptions<TData, TVariables>
-): QueryResult<TData> {
+) {
     const result = useApolloQuery<TData, TVariables>(query, options);
 
     if (result.error) {
