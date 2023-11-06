15cd632037ceba5594d23c1c372c2ee1cceefc92
Arpit Singh <arpit@secured-finance.com>
2023-11-06 16:05:20 +0530

    feat: use alchemy rpc url for public client [SF-726]

--- packages/sf-client/src/test/client.ts
+++ packages/sf-client/src/test/client.ts
@@ -1,17 +1,17 @@
 // Run this client to retrieve smart contract data
-// SF_ENV=development node packages/sf-client/dist/test/client.js
+// SF_ENV=development ALCHEMY_API_KEY=xxx node packages/sf-client/dist/test/client.js
 import { createPublicClient, http } from 'viem';
 import { sepolia } from 'viem/chains';
 import { SecuredFinanceClient } from '../secured-finance-client';
+
+const rpcUrl = `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`;
 
 const sfClient = new SecuredFinanceClient();
 
 const publicClient = createPublicClient({
     chain: sepolia,
-    transport: http(),
+    transport: http(rpcUrl),
 });
-
-const account = '0x'; // insert your account here
 
 sfClient.init(publicClient).then(() => {
     sfClient.getProtocolDepositAmount().then(res => console.log(res));
