506a3292f403348d422d72f561dc5c6584308704
Reuven Aboulker <reuven.aboulker@gmail.com>
2023-12-06 21:39:40 +0000

    doc: add token vault documentation [SF-937]

--- packages/sf-client/src/contracts/TokenVault.ts
+++ packages/sf-client/src/contracts/TokenVault.ts
@@ -14,6 +14,11 @@ export class TokenVault extends BaseContract {
         }
     }
 
+    /**
+     * Retrieves the token address for the specified currency.
+     * @param currency The currency for which to retrieve the token address.
+     * @returns A Promise that resolves to the token address.
+     */
     async getTokenAddress(currency: Currency) {
         return this.publicClient.readContract({
             ...this.getAbi(),
@@ -22,6 +27,11 @@ export class TokenVault extends BaseContract {
         });
     }
 
+    /**
+     * Retrieves the collateral parameters from the token vault contract.
+     * The collateral parameters include the liquidation threshold rate, liquidation protocol fee rate, and liquidator fee rate.
+     * @returns An object containing the liquidation threshold rate, liquidation protocol fee rate, and liquidator fee rate.
+     */
     async getCollateralParameters() {
         let result: readonly [bigint, bigint, bigint];
         switch (this.config.env) {
@@ -46,6 +56,13 @@ export class TokenVault extends BaseContract {
         };
     }
 
+    /**
+     * Retrieves the withdrawable collateral for a given currency and account.
+     *
+     * @param ccy - The currency for which to retrieve the withdrawable collateral.
+     * @param account - The account for which to retrieve the withdrawable collateral.
+     * @returns A Promise that resolves to the withdrawable collateral amount.
+     */
     async getWithdrawableCollateral(ccy: Currency, account: string) {
         switch (this.config.env) {
             case 'development':
@@ -64,6 +81,13 @@ export class TokenVault extends BaseContract {
         }
     }
 
+    /**
+     * Withdraws collateral from the token vault.
+     *
+     * @param ccy - The currency of the collateral.
+     * @param amount - The amount of collateral to withdraw.
+     * @returns A promise that resolves when the withdrawal is successful.
+     */
     async withdrawCollateral(ccy: Currency, amount: bigint) {
         const [address] = await this.walletClient.getAddresses();
 
@@ -106,6 +130,12 @@ export class TokenVault extends BaseContract {
         }
     }
 
+    /**
+     * Retrieves the used currencies for a specific account.
+     *
+     * @param account - The account for which to retrieve the used currencies.
+     * @returns A promise that resolves to the used currencies.
+     */
     async getUsedCurrencies(account: string) {
         switch (this.config.env) {
             case 'development':
@@ -124,6 +154,12 @@ export class TokenVault extends BaseContract {
         }
     }
 
+    /**
+     * Retrieves the total unused collateral amount for a given account.
+     *
+     * @param account - The account for which to retrieve the total unused collateral amount.
+     * @returns A promise that resolves to the total unused collateral amount.
+     */
     async getTotalUnusedCollateralAmount(account: string) {
         switch (this.config.env) {
             case 'development':
@@ -142,6 +178,12 @@ export class TokenVault extends BaseContract {
         }
     }
 
+    /**
+     * Retrieves the collateral book for a given account.
+     *
+     * @param account - The account for which to retrieve the collateral book.
+     * @returns An object containing the collateral book information, including collateral amounts, coverage, total collateral amount, and total unused collateral amount.
+     */
     async getCollateralBook(account: string) {
         const currencies = await this.getUsedCurrencies(account);
         let collateral: Record<string, bigint> = {};
@@ -200,6 +242,12 @@ export class TokenVault extends BaseContract {
         };
     }
 
+    /**
+     * Retrieves the coverage for a specific account.
+     *
+     * @param account - The account for which to retrieve the coverage.
+     * @returns A promise that resolves to the coverage value.
+     */
     async getCoverage(account: string) {
         switch (this.config.env) {
             case 'development':
@@ -218,6 +266,12 @@ export class TokenVault extends BaseContract {
         }
     }
 
+    /**
+     * Retrieves the total deposit amount for a given currency.
+     *
+     * @param currency - The currency for which to retrieve the total deposit amount.
+     * @returns A promise that resolves to the total deposit amount.
+     */
     async getTotalDepositAmount(currency: Currency) {
         switch (this.config.env) {
             case 'development':
@@ -236,6 +290,12 @@ export class TokenVault extends BaseContract {
         }
     }
 
+    /**
+     * Retrieves the total collateral amount for a given account.
+     *
+     * @param account - The account for which to retrieve the total collateral amount.
+     * @returns A promise that resolves to the total collateral amount.
+     */
     async getTotalCollateralAmount(account: string) {
         switch (this.config.env) {
             case 'development':
@@ -254,6 +314,13 @@ export class TokenVault extends BaseContract {
         }
     }
 
+    /**
+     * Retrieves the borrowable amount for a given account and currency.
+     *
+     * @param account - The account for which to retrieve the borrowable amount.
+     * @param currency - The currency for which to retrieve the borrowable amount.
+     * @returns A Promise that resolves to the borrowable amount.
+     */
     async getBorrowableAmount(account: string, currency: Currency) {
         switch (this.config.env) {
             case 'development':
