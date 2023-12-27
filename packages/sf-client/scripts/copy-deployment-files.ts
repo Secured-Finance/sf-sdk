22002cf7bbf2399eff281fa097335f9b58838ad8
akihiro tanaka <big.a.0816@gmail.com>
2023-12-27 21:55:26 +0900

    feat: support Arbitrum [SF-970]

--- packages/sf-client/scripts/copy-deployment-files.ts
+++ packages/sf-client/scripts/copy-deployment-files.ts
@@ -1,69 +1,93 @@
 import {
-    cpSync,
     existsSync,
     mkdirSync,
-    readdirSync,
     readFileSync,
+    readdirSync,
     rmSync,
-    statSync,
     unlinkSync,
     writeFileSync,
 } from 'fs';
-import { contractsList } from '../contractsList';
-
-function extractAddressAndABI(filePath: string) {
-    const fileContent = readFileSync(filePath, 'utf-8');
-    const contractData = JSON.parse(fileContent);
-    const { address, abi } = contractData;
-    const addressStr = address
-        ? 'export const address = ' + JSON.stringify(address, null, 2) + ';'
-        : '';
-    const abiStr = abi
-        ? 'export const abi = ' + JSON.stringify(abi, null, 2) + ' as const;'
-        : '';
-    const newContent =
-        addressStr && abiStr ? addressStr + '\n' + '\n' + abiStr : '';
-    writeFileSync(filePath.replace('.json', '.ts'), newContent);
-}
-
-function extractAddressesFromDeployments(deploymentDir: string) {
-    const files = readdirSync(deploymentDir);
-    for (const file of files) {
-        const filePath = `${deploymentDir}/${file}`;
-        const stat = statSync(filePath);
-        if (stat.isDirectory()) {
-            extractAddressesFromDeployments(filePath);
-        } else {
-            contractsList.every(value => {
-                if (filePath.includes(value)) {
-                    extractAddressAndABI(filePath);
-                    return false;
-                }
-                return true;
-            });
-            unlinkSync(filePath);
-        }
-    }
-}
+import { contractsList } from './constants';
 
 class Main {
     run() {
         const rootDir = process.cwd();
-        const modulePath = require
+        const moduleDir = require
             .resolve('@secured-finance/contracts/package.json')
-            .replace('/package.json', '');
-
+            .replace('/package.json', '/deployments');
         const deploymentDir = `${rootDir}/src/deployments`;
+
         if (existsSync(deploymentDir)) {
             rmSync(deploymentDir, { recursive: true });
         }
         mkdirSync(deploymentDir);
 
-        cpSync(`${modulePath}/deployments/`, deploymentDir, {
-            recursive: true,
-        });
+        this.extractAddressesFromDeployments(moduleDir, deploymentDir);
+    }
 
-        extractAddressesFromDeployments(deploymentDir);
+    private extractAddressesFromDeployments(
+        moduleDir: string,
+        deploymentDir: string
+    ) {
+        const networks = readdirSync(moduleDir);
+
+        for (const network of networks) {
+            for (const contract of contractsList) {
+                const file = `${contract}.json`;
+                const modulePath = `${moduleDir}/${network}/${file}`;
+                const filePath = `${deploymentDir}/${file}`;
+                this.extractAddressAndABI(modulePath, filePath, network);
+            }
+        }
+
+        this.writeFiles(deploymentDir);
+    }
+
+    private extractAddressAndABI(
+        modulePath: string,
+        filePath: string,
+        network: string
+    ) {
+        let addresses: Record<string, any> = {};
+        let abi: Record<string, any> = [];
+
+        if (existsSync(filePath)) {
+            const fileContent = readFileSync(filePath, 'utf-8');
+            ({ addresses, abi } = JSON.parse(fileContent));
+            rmSync(filePath);
+        }
+
+        if (existsSync(modulePath)) {
+            const moduleContent = readFileSync(modulePath, 'utf-8');
+            ({ address: addresses[network], abi } = JSON.parse(moduleContent));
+        } else {
+            addresses[network] = '';
+        }
+
+        writeFileSync(filePath, JSON.stringify({ addresses, abi }, null, 2));
+    }
+
+    private writeFiles(deploymentDir: string) {
+        const files = readdirSync(deploymentDir);
+
+        for (const file of files) {
+            const filePath = `${deploymentDir}/${file}`;
+            const fileContent = readFileSync(filePath, 'utf-8');
+            const { addresses, abi } = JSON.parse(fileContent);
+
+            const addressStr =
+                'export const addresses = ' +
+                JSON.stringify(addresses, null, 2) +
+                ';';
+            const abitr =
+                'export const abi = ' +
+                JSON.stringify(abi, null, 2) +
+                ' as const;';
+            const newContent = addressStr + '\n' + '\n' + abitr;
+
+            writeFileSync(filePath.replace('.json', '.ts'), newContent);
+            unlinkSync(filePath);
+        }
     }
 }
 
