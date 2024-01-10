911d4597874b16a5098afb7ba4aba74c9531f0fc
akihiro tanaka <big.a.0816@gmail.com>
2024-01-10 11:53:48 +0900

    feat: support Arbitrum [Sf-985]

--- packages/sf-graph-client/src/components/graphApolloClient.ts
+++ packages/sf-graph-client/src/components/graphApolloClient.ts
@@ -6,13 +6,14 @@ import {
 } from '@apollo/client';
 import { GraphApolloLink } from '@graphprotocol/client-apollo';
 
+import * as GraphClientArbitrumOne from '../graphclients/arbitrum-one/.graphclient';
+import * as GraphClientArbitrumSepolia from '../graphclients/arbitrum-sepolia/.graphclient';
 import * as GraphClientDevArb from '../graphclients/development-arb/.graphclient';
 import * as GraphClientDev from '../graphclients/development/.graphclient';
 import * as GraphClientMainnet from '../graphclients/mainnet/.graphclient';
 import * as GraphClientSepolia from '../graphclients/sepolia/.graphclient';
 import * as GraphClientStgArb from '../graphclients/staging-arb/.graphclient';
 import * as GraphClientStg from '../graphclients/staging/.graphclient';
-// import * as GraphClientArbitrumOne from '../graphclients/arbitrum-one/.graphclient';
 
 const environments = ['development', 'staging', 'production'] as const;
 type Environments = (typeof environments)[number];
@@ -59,8 +60,10 @@ const getGraphClient = (network = 'none') => {
                 GraphClient = GraphClientMainnet;
             } else if (network === 'sepolia') {
                 GraphClient = GraphClientSepolia;
-                // } else if (network === 'arbitrum-one') {
-                //     GraphClient = GraphClientArbitrumOne;
+            } else if (network === 'arbitrum-one') {
+                GraphClient = GraphClientArbitrumOne;
+            } else if (network === 'arbitrum-sepolia') {
+                GraphClient = GraphClientArbitrumSepolia;
             } else {
                 GraphClient = GraphClientMainnet;
                 sfNetwork = 'mainnet';
